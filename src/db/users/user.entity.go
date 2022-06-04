380916833ddcbae82ec07304bbaa81ecc3e07feb
Phùng Văn An <44137399+batthanhvan@users.noreply.github.com>
2022-06-05 01:19:02 +0700

    fixed: Auth: temporarily Ok (#14)

--- src/db/users/user.entity.go
+++ src/db/users/user.entity.go
@@ -8,5 +8,5 @@ type User struct {
 	gorm.Model
 	Username string `gorm:"size:255;not null;unique" json:"username"`
 	Password string `gorm:"size:255;not null;" json:"password"`
-	Role     string `gorm:"size:255;not null;" json:"role"`
+	Role     int32  `gorm:"type:int8;default:0"`
 }
--- src/lib/const.go
+++ src/lib/const.go
@@ -1,28 +1,51 @@
 package lib
 
-// const (
-// 	OTP_TYPE_REGISTER = iota
-// 	OTP_TYPE_FORGOT_PASSWORD
-// )
-
-// const (
-// 	SERVICE_STATUS_ACTIVE = iota
-// 	SERVICE_STATUS_INACTIVE
-// )
-
-// const (
-// 	PRESIGNED_URL_EXPIRE_TIME = 5 * time.Minute
-// )
-
-var (
-	SERVICE_NAME = "api-service"
-	DRIVER_NAME  = "mysql"
-	// ID           int64 = 2
-	// BUY_ADVERTISE_LIMIT int64 = 3
+const (
+	DRIVER_NAME string = "mysql"
 )
 
-// const (
-// 	ROLE_CUSTOMER = iota
-// 	ROLE_HANDYMAN
-// 	ROLE_ADMIN
+type ROLE int32
+
+const (
+	ROLE_PLAYER ROLE = 0
+	ROLE_ADMIN  ROLE = 1
+)
+
+// Enum value maps for ROLE.
+// var (
+// 	ROLE_name = map[int32]string{
+// 		0: "PLAYER",
+// 		1: "ADMIN",
+// 	}
+// 	ROLE_value = map[string]int32{
+// 		"PLAYER": 0,
+// 		"ADMIN":  1,
+// 	}
 // )
+
+// func (x ROLE) Enum() *ROLE {
+// 	p := new(ROLE)
+// 	*p = x
+// 	return p
+// }
+
+// func (x ROLE) String() string {
+// 	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+// }
+
+// func (ROLE) Descriptor() protoreflect.EnumDescriptor {
+// 	return file_const_proto_enumTypes[2].Descriptor()
+// }
+
+// func (ROLE) Type() protoreflect.EnumType {
+// 	return &file_const_proto_enumTypes[2]
+// }
+
+// func (x ROLE) Number() protoreflect.EnumNumber {
+// 	return protoreflect.EnumNumber(x)
+// }
+
+// // Deprecated: Use ROLE.Descriptor instead.
+// func (ROLE) EnumDescriptor() ([]byte, []int) {
+// 	return file_const_proto_rawDescGZIP(), []int{2}
+// }
